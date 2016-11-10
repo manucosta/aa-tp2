ad59136b2f0323cfba7880bcd4f6e40f68c4bea5
Federico Bertero <fedebertero@Federicos-MacBook-Pro.local>
2016-11-10 16:22:57 -0300

    Alternativa al diccionario y agrego comentarios poco utiles

--- src/4inline.py
+++ src/4inline.py
@@ -3,7 +3,13 @@ import random
 
 class FourInLine:
     def __init__(self, playerR, playerY):
-        self.board = [[' ',' ',' ',' ',' ',' '],[' ',' ',' ',' ',' ',' '],[' ',' ',' ',' ',' ',' '],[' ',' ',' ',' ',' ',' '],[' ',' ',' ',' ',' ',' '],[' ',' ',' ',' ',' ',' '],[' ',' ',' ',' ',' ',' ']]
+        self.board = [[' ',' ',' ',' ',' ',' '],
+                      [' ',' ',' ',' ',' ',' '],
+                      [' ',' ',' ',' ',' ',' '],
+                      [' ',' ',' ',' ',' ',' '],
+                      [' ',' ',' ',' ',' ',' '],
+                      [' ',' ',' ',' ',' ',' '],
+                      [' ',' ',' ',' ',' ',' ']]
         self.last = [0] * 7
         self.playerR, self.playerY = playerR, playerY
         self.playerR_turn = random.choice([True, False])
@@ -11,54 +17,58 @@ class FourInLine:
     def play_game(self):
         self.playerR.start_game('R')
         self.playerY.start_game('Y')
-        i = 0
+        iter = 0
         while True: #yolo
-
+            # Turn selection
             if self.playerR_turn:
                 player, char, other_player = self.playerR, 'R', self.playerY
             else:
                 player, char, other_player = self.playerY, 'Y', self.playerR
             if player.breed == "human":
                 self.display_board()
-            column = player.move(self.last, self.board)-1
-            if self.last[column] >= 6: # illegal move
-                print "Perdi por cagon"
+            # Based on the state, select an action
+            sel_column = player.move(self.last, self.board)-1
+            # Check if the selected action is ilegal
+            if self.last[sel_column] >= 6:
+                print "Accion ilegal!"
                 player.reward(-99, self.board) # score of shame
                 break
-            self.board[column][self.last[column]] = char
-            self.last[column] += 1
-            if self.player_wins(char,column):
+            # Is not ilegal, so lets update
+            self.board[sel_column][self.last[sel_column]] = char
+            self.last[sel_column] += 1
+            # Check if the actual player wins
+            if self.player_wins(char, sel_column):
                 player.reward(1, self.board)
                 other_player.reward(-1, self.board)
                 break
-            if self.board_full(): # tie game
-                print "ya PERDIMOS"
+            # Tie game?
+            if self.board_full():
+                print "Empate"
                 player.reward(0.5, self.board)
                 other_player.reward(0.5, self.board)
                 break
             other_player.reward(0, self.board)
+            # Swicht of turn
             self.playerR_turn = not self.playerR_turn
 
     def player_wins(self, char, column):
         row = self.last[column]-1
-        # Chequeamos columna
+        # Check by columns
         if row >= 3:
           if self.board[column][row] == self.board[column][row-1] == self.board[column][row-2] == self.board[column][row-3]:
-            print "ya gane por columnas", char
-            print "ultima: ", column
+            print "Gano el jugador", char, "por columna", column
             return True
-        # Chequeamos fila
+        # Check by rows
         counter = 0
         for col in xrange(7):
             if self.board[col][row] == char:
               counter += 1
               if counter == 4:
-                print "ya gane por filas", char
-                print "ultima: ", column
-                return True
+                  print "Gano el jugador", char, "por fila", column
+                  return True
             else:
               counter = 0
-        # TODO: Chequeamos diagonales
+        # TODO: Check by diagonals
 
         return False
 
@@ -103,45 +113,63 @@ class RandomPlayer(Player):
     def __init__(self):
         self.breed = "random"
 
-
     def start_game(self, char):
         pass
 
     def move(self, last, board):
         return random.choice(self.available_moves(last))
 
+
 class QLearningPlayer(Player):
     def __init__(self, epsilon=0.2, alpha=0.3, gamma=0.9):
         self.breed = "Qlearner"
         self.harm_humans = False
-        self.q = {} # (state, action) keys: Q values
+        # self.q = {} # (state, action) keys: Q values
+        self.q = [] # [((state, action), Q_values)]
         self.epsilon = epsilon # e-greedy chance of random exploration
         self.alpha = alpha # learning rate
         self.gamma = gamma # discount factor for future rewards
 
     def start_game(self, char):
         aux = [' ']*6
-        self.last_board = [aux] * 7
+        self.last_board = [aux]*7
         self.last_move = None
 
     def getQ(self, state, action):
         # encourage exploration; "optimistic" 1.0 initial values
-        if self.q.get((state, action)) is None:
-            self.q[(state, action)] = 1.0
-        return self.q.get((state, action))
+        # TODO: Probar "pesimista": 0.0 initial values.
+        return_val = None
+        
+        q_aux = [(s, a) for ((s, a), _) in self.q]
+        if not (state, action) in q_aux:
+            self.q.append(((state, action), 1.0))
+        
+        for ((s, a), q_val) in self.q:
+            if (s, a) == (state, action):
+                return_val = q_val
+                break
+
+        return return_val
+            
+        #if self.q.get((state, action)) is None:
+        #    self.q[(state, action)] = 1.0
+        #return self.q.get((state, action))
 
     def move(self, last, board):
-        aux = board
-        for t in xrange(0,len(aux)):
-          aux[t] = tuple(aux[t])
-        self.last_board = tuple(aux)
+        # aux = board
+        # for t in xrange(0,len(aux)):
+        #  aux[t] = tuple(aux[t])
+        self.last_board = tuple(board)
         actions = self.available_moves(last)
 
         if random.random() < self.epsilon: # explore!
             self.last_move = random.choice(actions)
             return self.last_move
-
+        
+        print 'last board', self.last_board
+        print 'actions', actions
         qs = [self.getQ(self.last_board, a) for a in actions]
+        print 'qs', qs
         maxQ = max(qs)
 
         if qs.count(maxQ) > 1:
